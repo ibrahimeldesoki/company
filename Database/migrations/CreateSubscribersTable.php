5b84d4b9c8cdbc31ef701c5591d30d21e45ff3a8
ibrahimeldesoki <ibrahim.eldesoki7@gmail.com>
2023-01-07 14:43:44 +0200

    status

--- Database/migrations/CreateSubscribersTable.php
+++ Database/migrations/CreateSubscribersTable.php
@@ -12,6 +12,7 @@ class CreateSubscribersTable implements DBInterface
         return 'CREATE TABLE subscrsibers (
                     id int (11) not null AUTO_INCREMENT,
                     `user_id` int (6) not null,
+                     status ENUM(`pending`, `active`, `inactive`) NOT NULL DEFAULT `pending`,
                     FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
                     `company_id` int (6),
                     FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`) ,
