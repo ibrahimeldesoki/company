309ef2403de87f7795b6cecd50ff66fb8d19096f
ibrahimeldesoki <ibrahim.eldesoki7@gmail.com>
2023-01-08 06:12:46 +0200

    migration

---
+++ Database/migrations/CreateInvoiceTable.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\migrations;
+
+use app\Interfaces\DBInterface;
+
+class CreateInvoiceTable implements DBInterface
+{
+
+    public function migrate(): string
+    {
+        return 'CREATE TABLE invoices (
+                    `id` int (11) not null primary key AUTO_INCREMENT,
+                    `reference_number` varchar(20) not null unique,
+                    `start_at` timestamp,
+                    `end_at` timestamp,
+                    `company_id` int (11),
+                    `total_price` int (11) not null,
+                    `active_count` int(11) not null,
+                    `pending_count` int (11) not null, 
+                     `company_id` int (6),
+                    FOREIGN KEY (`company_id`) REFERENCES `companies` (`id`)
+                )';
+    }
+}
