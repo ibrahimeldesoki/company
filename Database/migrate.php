523f0767a0408e210d0013c13c6fb8e16440d6b3
ibrahimeldesoki <ibrahim.eldesoki7@gmail.com>
2023-01-12 00:39:07 +0200

    autoload for migrate

--- Database/migrate.php
+++ Database/migrate.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace Database;
-require_once __DIR__ . '/../autoload.php';
+require_once __DIR__.'/../vendor/autoload.php';
 
 use App\core\DataBaseConnection;
 use App\core\MigrationTable;
